82d7c5ea8059dfff44dd9d6ecbe514e917ca529d
Danny <danny.p@anzuk.education>
2018-04-13 14:53:02 +1000

    Started again, without Redux template

---
+++ DanzNet2/ClientApp/components/book/BooksContainer.tsx
@@ -0,0 +1,38 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import bookApi from 'ClientApp/api/mockBookApi';
+import { BookModel } from '../book/models';
+
+interface BooksState {
+    Books: BookModel[];
+}
+
+export class BooksContainer extends React.Component<RouteComponentProps<{}>, BooksState> {
+    constructor() {
+        super();
+        this.state = { Books: [] };
+
+        this.fetchBooks();
+    }
+
+    public render() {
+        return <div>
+            <h1>Counter</h1>
+
+            <p>This is a simple example of a React component.</p>
+
+          
+
+           
+        </div>;
+    }
+
+    fetchBooks() {
+        return bookApi.getAll()
+            .then(books => {
+                
+            }).catch(error => {
+                throw (error);
+            });
+    }
+}
